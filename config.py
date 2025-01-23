d801ce52ac3150d2fa33ccf9cd425040f19df88e
Luis Mori Guerra <luismorith@gmail.com>
2025-01-23 12:44:41 -0500

    feat: updated env vars
    Signed-off-by: Luis Mori Guerra <luismorith@gmail.com>

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# Load .env file only in development
+if os.getenv("RENDER") is None:  # RENDER is an environment variable automatically set by Render.com
+    load_dotenv()
 
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY") 
+
